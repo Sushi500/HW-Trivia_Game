0a8e4a0968ebf2d801915f9bf0ed538d8ff25915
Susanta Mozumder <sushanta.mozumder@gmail.com>
2018-06-20 22:36:03 -0400

    html&css

--- Assets/Javascript/app.js
+++ Assets/Javascript/app.js
@@ -1,107 +1,187 @@
-var userPick;
-
-var correctAnswer = 0;
-
-var incorrectAnswer = 0;
-
-var unAnswer = 0;
-
-var question = 0;
-
-var images;
-
-var count=30;
-
-var disneyQuestion = [{
-question: "In Aladdin, what is the name of Jasmine's pet tiger?",
-choices: ["Rajah", "Bo", "Iago", "Jack" ],
-images:  ["../images/Rajah.gif"],
-validAnswer: 0
-}, {
-question:"In Peter Pan, Captain Hook had a hook on which part of his     body?",
-choices: ["Right Foot", "Left Hand", "Left Foot", "Right Hand"],
-validAnswer: 1
-
-}, {
-question:"In the Lion King, where does Mufasa and his family live?",
-choices: ["Rocky Mountain", "Forest", "Desert", "Pride Rock"],
-validAnswer: 3
-
-}, {
-question:"In Beauty and the Beast, how many eggs does Gaston eat for    breakfast?",
-choices: ["2 Dozen", "5 Dozen", "5000", "0"],
-validAnswer: 1
-
-}, {
-question:"In Alice in Wonderland, what is the name of Alice’s kitten?",
-choices: ["Dinah", "Sammie", "Kat", "Luna"],
-validAnswer: 0
-
- }, {
-question:"After being on earth, where did Hercules first meet his   father Zeus?",
-choices: ["Mount Olympus", "Greece", "In the Temple of Zeus", "Elysian   Fields"],
-validAnswer: 2
-
-}, {
-question:"During the ballroom scene of Beauty & the Beast, what color is Belle’s Gown?",
-choices: ["Yellow", "Blue", "Gold", "White"],
-validAnswer: 2
-
-}, {
-question:"In Bambi, what word does the owl use to describe falling in love?",
-choices: ["Whimsical", "Miserable", "Joyful", "Twitterpatted"],
-validAnswer: 3
-
-}
-
-];
-
-$("#start_button").click(function(){
-$(this).hide();
-counter = setInterval(timer, 1000); 
-displayTrivia();
-}); 
-
-
-function timer(){
-count--;
-if (count <= 0) {
- clearInterval(counter);
- return;
-}
-
- $("#timer").html("Time remaining: " + "00:" + count + " secs");
-}
-
-
-function displayTrivia() {
-$("#question_div").html(disneyQuestion[0].question);
-question++;
-
-  var choicesArr = disneyQuestion[0].choices;
-  var buttonsArr = [];
-
-  for (let i = 0; i < choicesArr.length; i++) {
-    var button = $('<button>');
-    button.text(choicesArr[i]);
-    button.attr('data-id', i);
-    $('#choices_div').append(button);
-   }
-
-  } 
-
- $('#choices_div').on('click', 'button', function(e){
- userPick = $(this).data("id");
- disneyQuestion[0].validAnswer;
- if(userPick != disneyQuestion[0].validAnswer) {
-
- $('#choices_div').text("Wrong Answer! The correct answer is Rajah.");
- incorrectAnswer++;
-
-} else if (userPick === disneyQuestion[0].validAnswer) {
-$('#choices_div').text("Correct!!! The pet tiger name is Rajah");
-correctAnswer++;
-
-}
-
-});
+$(document).ready(function () {
+  
+  var marvelQuestion = [{
+    question: "Dr. Bruce Banner is also known as waht hero?",
+    choices: ["The Hulk", "Thor", "Dr. Strange", "Deadpool" ],
+    validAnswer: 0
+    }, {
+    question:"What is Thor's weapon of choice?",
+    choices: ["An Axe", "A Hammer", "A Cane", "Rocket Launcher"],
+    validAnswer: 1
+    
+    }, {
+    question:"Captain America fought in what war?",
+    choices: ["World War 1", "Civil War", "Vietnam War" , "World War 2"],
+    validAnswer: 3
+    
+    }, {
+    question:"How many Infinity Stones are there?",
+    choices: ["2", "5", "7", "9"],
+    validAnswer: 1
+    
+    }, {
+    question:"Scarlet Witch is in a relationshop with what hero?",
+    choices: ["Vision", "Star Lord", "Hawkeye", "Falcon"],
+    validAnswer: 0
+    
+     }, {
+    question:"Groot is a member of what group?",
+    choices: ["The Avengers", "Hydra", "The Gurdians of the Galaxy", "The Defenders"],
+    validAnswer: 2
+    
+    }, {
+    question:"What color is the Power Stone?",
+    choices: ["Yellow", "Blue", "Purple", "White"],
+    validAnswer: 2
+    
+    }, {
+    question:"Who is Loki's adopted father?",
+    choices: ["Thor", "Heimdall", "Surtur", "Odin"],
+    validAnswer: 3
+    }];
+  
+  var correctCount = 0;
+  var wrongCount = 0;
+  var unanswerCount = 0;
+  var timer = 20;
+  var intervalId;
+  var userGuess ="";
+  var running = false;
+  var qCount = options.length;
+  var pick;
+  var index;
+  var newArray = [];
+  var holder = [];
+  
+  
+  
+  $("#reset").hide();
+  //click start button to start game
+  $("#start").on("click", function () {
+      $("#start").hide();
+      displayQuestion();
+      runTimer();
+      for(var i = 0; i < choices.length; i++) {
+    holder.push(options[i]);
+  }
+    })
+  //timer start
+  function runTimer(){
+    if (!running) {
+    intervalId = setInterval(decrement, 1000); 
+    running = true;
+    }
+  }
+  //timer countdown
+  function decrement() {
+    $("#timeleft").html("<h3>Time remaining: " + timer + "</h3>");
+    timer --;
+  
+    //stop timer if reach 0
+    if (timer === 0) {
+      unanswerCount++;
+      stop();
+      $("#answerblock").html("<p>Time is up! The correct answer is: " + pick.choice[pick.answer] + "</p>");
+      hidepicture();
+    }	
+  }
+  
+  //timer stop
+  function stop() {
+    running = false;
+    clearInterval(intervalId);
+  }
+  //randomly pick question in array if not already shown
+  //display question and loop though and display possible answers
+  function displayQuestion() {
+    //generate random index in array
+    index = Math.floor(Math.random()*choices.length);
+    pick = choices[index];
+  
+  //	if (pick.shown) {
+  //		//recursive to continue to generate new index until one is chosen that has not shown in this game yet
+  //		displayQuestion();
+  //	} else {
+  //		console.log(pick.question);
+      //iterate through answer array and display
+      $("#questionblock").html("<h2>" + pick.question + "</h2>");
+      for(var i = 0; i < pick.choice.length; i++) {
+        var userChoice = $("<div>");
+        userChoice.addClass("answerchoice");
+        userChoice.html(pick.choice[i]);
+        //assign array position to it so can check answer
+        userChoice.attr("data-guessvalue", i);
+        $("#answerblock").append(userChoice);
+  //		}
+  }
+  
+  
+  
+  //click function to select answer and outcomes
+  $(".answerchoice").on("click", function () {
+    //grab array position from userGuess
+    userGuess = parseInt($(this).attr("data-guessvalue"));
+  
+    //correct guess or wrong guess outcomes
+    if (userGuess === pick.answer) {
+      stop();
+      correctCount++;
+      userGuess="";
+      $("#answerblock").html("<p>Correct!</p>");
+      hidepicture();
+  
+    } else {
+      stop();
+      wrongCount++;
+      userGuess="";
+      $("#answerblock").html("<p>Wrong! The correct answer is: " + pick.choice[pick.answer] + "</p>");
+      hidepicture();
+    }
+  })
+  }
+  
+  
+  function hidepicture () {
+    $("#answerblock").append("<img src=" + pick.photo + ">");
+    newArray.push(pick);
+    options.splice(index,1);
+  
+    var hidpic = setTimeout(function() {
+      $("#answerblock").empty();
+      timer= 20;
+  
+    //run the score screen if all questions answered
+    if ((wrongCount + correctCount + unanswerCount) === qCount) {
+      $("#questionblock").empty();
+      $("#questionblock").html("<h3>Game Over!  Here's how you did: </h3>");
+      $("#answerblock").append("<h4> Correct: " + correctCount + "</h4>" );
+      $("#answerblock").append("<h4> Incorrect: " + wrongCount + "</h4>" );
+      $("#answerblock").append("<h4> Unanswered: " + unanswerCount + "</h4>" );
+      $("#reset").show();
+      correctCount = 0;
+      wrongCount = 0;
+      unanswerCount = 0;
+  
+    } else {
+      runTimer();
+      displayQuestion();
+  
+    }
+    }, 3000);
+  
+  
+  }
+  
+  $("#reset").on("click", function() {
+    $("#reset").hide();
+    $("#answerblock").empty();
+    $("#questionblock").empty();
+    for(var i = 0; i < holder.length; i++) {
+      options.push(holder[i]);
+    }
+    runTimer();
+    displayQuestion();
+  
+  })
+  
+  })
